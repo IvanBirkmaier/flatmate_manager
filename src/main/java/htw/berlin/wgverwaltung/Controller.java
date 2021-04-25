a47c70995ee083559e1a49e12f3a3224465a6305
PascalHamar <s0573990@htw-berlin.de>
2021-04-25 16:46:11 +0200

    Hello World 2

--- src/main/java/htw/berlin/wgverwaltung/Controller.java
+++ src/main/java/htw/berlin/wgverwaltung/Controller.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package htw.berlin.wgverwaltung;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
