605809f7878644f416abddce8361a72d539a62e0
unknown <you@example.com>
2021-04-25 16:03:07 +0200

    Projek eingerichtet.

---
+++ src/main/java/htw/berlin/wgverwaltung/WgVerwaltungApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.wgverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgVerwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgVerwaltungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/wgverwaltung/WgVerwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.wgverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgVerwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
