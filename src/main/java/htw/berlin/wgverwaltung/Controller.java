f6865d13d513d08cedd0771978e171d2aa3a5235
unknown <you@example.com>
2021-04-29 13:38:39 +0200

    helloWorld change for merge

--- src/main/java/htw/berlin/wgverwaltung/Controller.java
+++ src/main/java/htw/berlin/wgverwaltung/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Controller {
 
-    @RequestMapping("/helloworld")
+    @RequestMapping("/helloWorld")
     public String index() {
         return "Greetings from Ivan!";
     }
