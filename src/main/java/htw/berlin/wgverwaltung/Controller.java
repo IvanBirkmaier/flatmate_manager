77600065d0188f6e37d0705522bb7af242b143e4
PascalHamar <s0573990@htw-berlin.de>
2021-06-09 15:46:46 +0200

    Authenti. Notation läuft OidcUser geht Fehlermeldung beim laden der Website

--- src/main/java/htw/berlin/wgverwaltung/Controller.java
+++ src/main/java/htw/berlin/wgverwaltung/Controller.java
@@ -1,17 +1,35 @@
 package htw.berlin.wgverwaltung;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
 @Autowired
 private Service service = new Service();
 
-@GetMapping("/home")
+    @GetMapping("/home")
+    public String getCurrentUser(@AuthenticationPrincipal OidcUser user, Model model) {
+        String email = user.getEmail();
+
+        model.addAttribute("email", email);
+        //model.addAttribute("lastAccess", usersLastAccess.get(email));
+        model.addAttribute("firstName", user.getGivenName());
+        model.addAttribute("lastName", user.getFamilyName());
+
+       // usersLastAccess.put(email, LocalDateTime.now());
+
+        return "home";
+    }
+
+@GetMapping("/startseite")
     public String startseite(Model model){
     List<Produkt> products = service.findeAlleOffenenProdukte();
     model.addAttribute("products", products);
