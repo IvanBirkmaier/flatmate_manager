272012f7378c4203d2d6746e22f601e5544bef7c
PascalHamar <s0573990@htw-berlin.de>
2021-06-28 20:30:38 +0200

    README + ServiceTest

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # Web_Tech_Projekt_WGVerwaltung
 
 ## **Beschreibung:**
-> Mit unserem Web-Tech Projekt wollen wir eine WG-Verwaltungs Website umsetzen, in der sich Wohngemeinschaften anmelden können und  gemeinsam
-> Einkaufslisten sowie Putzpläne erzeugen können. 
+> Die WG-Verwaltungs-App stellt ein Unterstützungstool für Wohnungsgemeinschaften dar, welches einem die Organisation der Einkäufe und generellen Themen 
+> ermöglicht.
+> 
+> Beim Aufrufen der Website wird der Nutzer automatisch auf eine Loginpage geleitet, auf der man sich einloggen bzw. bei Erstbenutzung
+> registrieren kann. Sobald der Login durchgeführt wurde, ist die Startseite zu sehen. Die Startseite bildet die gesamte Anwendung als Single-Page-Applikation.
+> 
+> Auf der linken Seite befindet sich die Einkaufsliste. Durch die Eingabe in das Textfeld und der Betätigung des "Hinzuüfgen"-Buttons lassen sich Artikel auf die Liste setzen.
+> Sobald ein Artikel gekauft wurde, kann man diesen durch Auswahl der Checkbox von der Liste nehmen. Nachdem das Häkchen bei einem Artikel gesetzt wurde, wird dieser in einem anderen Layout am Ende der Liste angezeigt.
+> Sobald man einen bereits gekauften Artikel erneut benötigt, muss man diesen nicht erneut anlegen, da man den Artikel durch die Checkbox wieder aktivieren kann. Durch den Button "Einkaufsliste leeren" lassen sich alle Artikel von der Liste löschen.
+> 
+> Auf der rechten Seite befindet sich eine To-Do-Liste, hier können Aufgaben durch die Eingabe ins Textfeld und Betätigung des "Hinzufügen"-Buttons erstellt werden.
+> Sobald eine Aufgabe erledigt wurde, kann man diese über den "X"-Button entfernen. Ist die To-Do-Liste leer, erscheint die Nachricht "alles erledigt :)"
+> 
+> In der rechten, oberen Ecke befindet sich der "Logout"-Button zum Abmelden von der Website.
 
-> Pascal Hamar s0573990, Ivan Birkmaier s057*420*4
+> Pascal Hamar s0573990, Ivan Birkmaier s0574204
 
 
--- src/test/java/htw/berlin/wgverwaltung/ServiceTest.java
+++ src/test/java/htw/berlin/wgverwaltung/ServiceTest.java
@@ -1,23 +1,20 @@
 package htw.berlin.wgverwaltung;
 
+import htw.berlin.wgverwaltung.persistence.PinnwandEntity;
 import htw.berlin.wgverwaltung.persistence.PinnwandRepository;
 import htw.berlin.wgverwaltung.persistence.ProduktEntity;
 import htw.berlin.wgverwaltung.persistence.ProduktRepository;
 import htw.berlin.wgverwaltung.service.Service;
 import org.junit.jupiter.api.Assertions;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.any;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.Rollback;
-
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
 
 @SpringBootTest
 public class ServiceTest {
@@ -48,38 +45,26 @@ public class ServiceTest {
         Assertions.assertSame(ownerProducts.get(0).getProductname(),"OwnerProduct", "Das Produkt des Users");
 
     }
-
-   /* @Test
-    @DisplayName("Soll alle Produkte eines Users löschen.")
-    void testDeleteAllProducts(){
-        var p1 = new ProduktEntity("OwnerProduct");
-        p1.setOwner("Owner");
-        var p2 = new ProduktEntity("OwnerProduct2");
-        p2.setOwner("Owner");
-
-
-        List<ProduktEntity> productList = service.findAll("Owner");
-        service.deleteAll("Owner");
-        assertThat(productList.isEmpty()==false);
-    }
-*/
-   /* @Test
-    @DisplayName("Soll ein Produkt mit einer bestimmten ID löschen.")
-    void testDeleteProductById(){
-        var p1 = new ProduktEntity("product1");
-        p1.setOwner("Owner");
-        p1.setProductId(123L);
-        var p2 = new ProduktEntity("product2");
-        p2.setOwner("Owner");
-        p2.setProductId(456L);
-
-
-
-        List<ProduktEntity> productList = service.findAll("Owner");
-        for (int i = 0, )
-        productList.add(service.findAll("Owner"));
-        assertThat(productList.isEmpty());
-*/
-
     @Test
+    @DisplayName("Soll alle Pinnwandeinträge eines Users anzeigen/filtern")
+   void testFindAllPinboard(){
+        String post = "Text to Show on the Board";
+        String owner = "Owner";
+        var pb1 = new PinnwandEntity();
+        pb1.setOwner(owner);
+        pb1.setPost(post);
+
+        var pb2 = new PinnwandEntity();
+        pb2.setOwner("NotOwner");
+        pb2.setPost("Lorem Ipsum");
+
+        doReturn(List.of(pb1,pb2)).when(pinnwandRepo).findAll();
+
+        List<PinnwandEntity> ownerPosts = service.findAllpinWall(owner);
+
+        Assertions.assertSame(ownerPosts.size(), 1, "Anzahl der Owner Posts");
+        Assertions.assertSame(ownerPosts.get(0).getPost(), post, "Owner Post");
+
+   }
+
 }
